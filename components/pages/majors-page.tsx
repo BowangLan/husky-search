dd7367e41df510f3efbec121b628bdad530a9332
Jeffrey Lan <blan2@uw.edu>
2025-08-08 11:13:02 -0700

    improved major search logic; removed unused skeleton

--- components/pages/majors-page.tsx
+++ components/pages/majors-page.tsx
@@ -25,11 +25,13 @@ export function MajorsPage({
   const [displayPrograms, setDisplayPrograms] = useState(programs)
 
   const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value
+    const value = e.target.value.toLowerCase()
     setQuery(value)
     setDisplayPrograms(
-      programs.filter((program) =>
-        program.title.toLowerCase().includes(value.toLowerCase())
+      programs.filter(
+        (program) =>
+          program.title.toLowerCase().includes(value) ||
+          program.code.toLowerCase().includes(value)
       )
     )
   }
@@ -85,43 +87,5 @@ export const MajorsPageSkeleton = () => {
         <div className="text-muted-foreground text-lg">Loading...</div>
       </div>
     </PageWithHeaderLayoutSkeleton>
-  )
-  return (
-    <PageWithHeaderLayout
-      title={
-        <div className="flex items-center gap-2 text-base text-muted-foreground font-light">
-          <Skeleton className="h-4 w-24" />
-        </div>
-      }
-      subtitle={
-        <div className="flex items-center gap-2 text-base text-muted-foreground font-light">
-          <Skeleton className="h-4 w-24" />
-        </div>
-      }
-    >
-      <section className="w-full px-page mx-page mb-6">
-        <div className="sticky top-16">
-          <div className="relative max-w-xs">
-            <Skeleton className="h-10 w-full" />
-          </div>
-        </div>
-      </section>
-      <section className="w-full px-page mx-page">
-        <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-          {Array.from({ length: 12 }).map((_, index) => (
-            <div key={index} className="relative">
-              <div className="rounded-lg border bg-card text-card-foreground shadow-sm">
-                <div className="p-6">
-                  <div className="h-12 md:h-20 flex flex-col justify-center items-center gap-1">
-                    <Skeleton className="h-6 w-24 mb-2" />
-                    <Skeleton className="h-4 w-32" />
-                  </div>
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
-      </section>
-    </PageWithHeaderLayout>
   )
 }
