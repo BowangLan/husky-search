f50bbf7e8fab39948f61380df50b7089cfc21895
Jeffrey Lan <blan2@uw.edu>
2025-12-18 20:58:48 -0800

    removed useless things

--- components/pages/gen-ed-detail-page.tsx
+++
@@ -1,5 +0,0 @@
-/**
- * @deprecated This file is deprecated. Import from "@/components/pages/gen-ed" instead.
- * This re-export is kept for backward compatibility.
- */
-export { GenEdDetailPage } from "./gen-ed"
